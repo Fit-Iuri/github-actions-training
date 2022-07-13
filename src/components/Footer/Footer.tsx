60a736cfddbf8f3a0f08482fa1595479a62b6f76
Fit-Iuri <iuri.mendes@fitbank.com.br>
2022-07-13 10:29:40 -0300

    add footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+export const Footer: React.FC = () => {
+  return (
+    <div>
+      <div>Footer</div>
+      <div>ano</div>
+      <div>Arroz</div>
+    </div>
+  );
+};
