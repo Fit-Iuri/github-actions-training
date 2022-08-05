f00b884fe51d28cacb6782140c18e96a259d0680
Fit-Iuri <iuri.mendes@fitbank.com.br>
2022-08-05 13:57:49 -0300

    122112

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -4,7 +4,7 @@ export const Home: React.FC = () => {
   return (
     <div>
       <div>Casa!</div>
-      <div>Testando GitHub Actions..</div>
+      <div>Testando GitHub Actions...</div>
       <div>Home</div>
       <div>Hora da verdade!</div>
     </div>
